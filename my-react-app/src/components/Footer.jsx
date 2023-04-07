25885650f03934a63cdb8c62314da0689993b94d
trista_lea <tristan_lea@outlook.com>
2023-04-07 10:44:50 -0700

    small stuff, forgot to push

--- my-react-app/src/components/Footer.jsx
+++ my-react-app/src/components/Footer.jsx
@@ -9,12 +9,12 @@ import "../assets/styles/Footer.css"
 export default function Footer() 
 {
   return (
-    <div>
+    <footer>
       <div className="footer">
         <a href="https://github.com/TriLea"><FaGithub size='30' /></a>
         <a href="https://www.linkedin.com/in/tristan-lea-710002230"><FaLinkedin size='30' /></a>
         <a href="https://profile.indeed.com/?hl=en_US&co=US&from=gnav-homepage&_ga=2.860660.412603641.1680489815-1971695938.1680133790"><SiIndeed size='30' /></a>
       </div>
-    </div>
+    </footer>
   )
 }
--- my-react-app/src/components/Header.jsx
+++ my-react-app/src/components/Header.jsx
@@ -3,11 +3,6 @@
 //what do i need here?
 // name and picture
 
-
-//path is wrong
-/* <img src={require('./assets/images/tristan_lea_paint_logo.png')} />
-<img src={require('./assets/images/me.png')} /> */
-
 import "../assets/styles/Header.css"
 
 export default function Header()
@@ -17,6 +12,9 @@ export default function Header()
             <div className=''>
                 <p>Header</p>
                 <h1>Tristan Lea</h1>
+                
+                <img className="img1" src={require('../assets/images/tristan_lea_paint_logo.png')} />
+                <img className="img2" src={require('../assets/images/me.png')} />
             </div>
         </header>
     );
