c069a75753a7b120cc152fba543518c24b23aa61
trista_lea <tristan_lea@outlook.com>
2023-04-19 14:12:40 -0700

    small changes to readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# React_Portfolio
+# React_Portfolio_prototype
+
+## Description
+just making a test portfolio before i go to make the real thing
+
+used react rounter to make the pages
+that was the biggest thing i learned, for making a working navigation bar
--- my-react-app/src/Pages/AboutMe.jsx
+++ my-react-app/src/Pages/AboutMe.jsx
@@ -7,14 +7,7 @@ export default function AboutMe()
     <div className="about">
         <h2> Hi, My Name is Tristan</h2>
         <div className="prompt">
-            <p>Hi I'm Tristan, a cs drop out (finacial reasons) I took this class
-                to distract my self from my mom's passing,
-                <br />and I have seemingly made it a lot further than I thought I would;
-                <br />
-                thanks for teaching me fullstack coding
-                UW bootcamp :D
-                <br />
-                I hope to find a job that will hellp me pay for my bachelors in cs!
+            <p>Hi I'm Tristan
             </p>
         </div>
     </div>
